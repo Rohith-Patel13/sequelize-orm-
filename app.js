d2ee742a67ee13c62cbc82a41bff54e8311a09ac
Rohith-Patel13 <rohithgithub999@gmail.com>
2023-12-16 19:35:11 +0530

    normal changes

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ app.use(express.json());
 
 const {sequelize,User} = require("./models");
 
+const http = require("http"); // Imports the built-in Node.js http module, which is used to create an HTTP server.
+const {Server} = require("socket.io"); // Destructures the Server class from the socket.io module. socket.io is a library for real-time web applications, and the Server class is used to create a WebSocket server.
+
+const cors = require('cors');
+app.use(cors()); // Enable CORS for all routes
+const server = http.createServer(app); // Creates an HTTP server using the Express application (app). This server will handle HTTP requests.
+
+const io = new Server(server,{
+    cors:{
+        origin:['http://localhost:3000']// client running url 
+    },
+}); // Creates a new instance of the Server class from socket.io and attaches it to the existing HTTP server (server). It also configures CORS for WebSocket connections. In this case, it allows connections from http://localhost:3000
+
+
 //create
 app.post("/users",async(req,res)=>{
     const {firstName,lastName,email}= req.body
@@ -57,3 +71,18 @@ app.listen(7000,async ()=>{
     console.log("database connected")
     await sequelize.authenticate() // await sequelize.authenticate() is used to ensure that the Sequelize ORM establishes a connection to the database before starting the Express.js server. If there is an issue with the database connection, an error will be logged to the console, and the server won't be started.
 })
+
+
+io.on("connection", (socket) => {
+    console.log("A user connected");
+  /*
+  EXAMPLE:
+    // Notify clients about changes in the database
+    Product.afterCreate((product) => {
+      io.emit("newProduct", product);
+    });
+  */
+    socket.on("disconnect", () => {
+      console.log("User disconnected");
+    });
+});
